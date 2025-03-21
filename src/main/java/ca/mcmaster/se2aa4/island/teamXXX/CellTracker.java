b7262407343a524cc4e2d30747e63a30c33a2b78
Ahmed <ah.yassin05@gmail.com>
2025-03-21 02:53:31 -0400

    Added necessary site methods

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/CellTracker.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/CellTracker.java
@@ -1,22 +1,64 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
-import java.util.ArrayList;
+import java.rmi.server.UID;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.xmlgraphics.ps.dsc.ResourceTracker;
-
-//import ca.mcmaster.se2aa4.island.teamXXX.Drone.Drone;
-//import ca.mcmaster.se2aa4.island.teamXXX.Response.ScanResponse;
+import org.apache.xmlgraphics.ps.dsc.CellTracker;
 
 public class CellTracker {
-    private static final Logger logger = LogManager.getLogger(CellTracker.class);
+
+    //inner class to represent x,y position
+    private static class Position {
+        double x;
+        double y;
+
+        Position(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     private static CellTracker instance;
-    
-    //private Map<String, > creeks;
-    //private Map<String, > emergencySites;
-    private Drone drone;
+
+    //maps from UID to position
+    private Map<String, Position> creeks = new HashMap<>();
+    private Map<String, Position> emergencySites = new HashMap<>();
+
+    private CellTracker() {}
+
+    public static synchronized CellTracker getInstance() {
+        if (instance == null) {
+            instance = new CellTracker();
+        }
+        return instance;
+    }
+
+    public void addCreek(String uid, double x, double y) {
+        creeks.put(uid, new Position(x, y));
+    }
+
+    public void addEmergencySite(String uid, double x, double y) {
+        emergencySites.put(uid, new Position(x, y));
+    }
+
+    public Set<String> getAllCreeks() {
+        return creeks.keySet();
+    }
+
+    public Set<String> getAllEmergencySites() {
+        return emergencySites.keySet();
+    }
+
+    public boolean hasCreek(String uid) {
+        return creeks.containsKey(uid);
+    }
+
+    public boolean hasEmergencySite(String uid) {
+        return emergencySites.containsKey(uid);
+    }
+
 }
